736961469ce27870a738b7b756603909d82ea9fe
Nikita <nikitakruglyakovwork@gmail.com>
2022-11-04 14:48:02 +0300

    feat: Implement auth middleware

---
+++ server/src/midlewares/auth-middleware.js
@@ -0,0 +1,29 @@
+// import { Request, Response, NextFunction } from 'express';
+
+import ApiError from '../exeptions/api-error';
+import tokenService from '../service/token-service';
+
+export function authMiddleware(req, res, next) {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+      return next(ApiError.UnauthorizedError());
+    }
+    const accessToken = authorizationHeader.split(' ')[1];
+
+    if (!accessToken) {
+      return next(ApiError.UnauthorizedError());
+    }
+    const userData = tokenService.validateAccessToken(accessToken);
+
+    if (!userData) {
+      return next(ApiError.UnauthorizedError());
+    }
+    req.user = userData;
+    next();
+  } catch (e) {
+    console.log('error');
+
+    return next(ApiError.UnauthorizedError());
+  }
+}
--- server/src/router/authRouter.ts
+++ server/src/router/authRouter.ts
@@ -1,13 +1,14 @@
 const Router = require('express').Router;
 import userController from '../controller/user-controller';
 import { body } from 'express-validator';
-const router = new Router();
+import { authMiddleware } from '../midlewares/auth-middleware';
 
+const router = new Router();
 router.post('/registration', body('email').isEmail(), body('password').isString().isLength({ min: 3, max: 64 }), userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
+router.get('/users', authMiddleware, userController.getUsers);
 
 export default router;
