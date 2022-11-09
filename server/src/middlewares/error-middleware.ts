c0b34dda72080bf4a1f7895a126dc4ae74b99fdb
Nikita <nikitakruglyakovwork@gmail.com>
2022-11-09 13:21:35 +0300

    feat: Some fixing

--- server/src/middlewares/error-middleware.ts
+++ server/src/middlewares/error-middleware.ts
@@ -2,11 +2,9 @@ import { Request, Response, NextFunction } from 'express';
 import ApiError from '../exeptions/api-error';
 import PhotoError from '../exeptions/photo-error';
 
-type TError = PhotoError | ApiError;
-
 export default function (err, req: Request, res: Response, next: NextFunction) {
   console.log(err);
-  if (err instanceof PhotoError || err instanceof PhotoError) {
+  if (err instanceof ApiError || err instanceof PhotoError) {
     return res.status(err.status).json({ message: err.message, errors: err.errors });
   }
   return res.status(500).json({ message: err.message ?? 'Непредвиденная ошибка' });
