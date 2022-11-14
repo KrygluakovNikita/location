59550fa1648e64c930dc0a8c59d529df8cc8f34b
Nikita <nikitakruglyakovwork@gmail.com>
2022-11-14 14:24:24 +0300

    fix: Fixed returned data for game

--- server/src/service/game-service.ts
+++ server/src/service/game-service.ts
@@ -1,9 +1,10 @@
 import { IGame } from './../interfaces/game-interface';
 import { Equal } from 'typeorm';
 import { Game, User } from '../database/entity';
+import { GameDto } from '../dtos/game-dto';
 
 class GameService {
-  async upload(data: IGame): Promise<Game> {
+  async upload(data: IGame): Promise<GameDto> {
     const game = new Game();
 
     const user = await User.findOneBy({ userId: data.userId });
@@ -14,22 +15,26 @@ class GameService {
     game.user = user;
 
     await game.save();
+    const result = new GameDto(game);
 
-    return game;
+    return result;
   }
 
-  async getGamesByUserId(userId: string): Promise<Game[]> {
+  async getGamesByUserId(userId: string): Promise<GameDto[]> {
     const user = await User.findOneBy({ userId });
 
-    const games = await Game.findBy({ user: Equal(user.userId) });
+    const games = await Game.find({ where: { user: Equal(user.userId) }, relations: { user: true } });
+    const result = games.map(game => new GameDto(game));
 
-    return games;
+    return result;
   }
 
-  async getAllGames(): Promise<Game[]> {
-    const games = await Game.find({ loadRelationIds: true });
+  async getAllGames(): Promise<GameDto[]> {
+    const games = await Game.find({ relations: { user: true } });
 
-    return games;
+    const result = games.map(game => new GameDto(game));
+
+    return result;
   }
 }
 
