218ce264ddf310d987dad009624a5ac18acc5067
KrygluakovNikita <nikita.kruglyakov@hf-solution.com>
2022-11-28 17:00:54 +0300

    feat: Implement login first page css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
-const express = require('express');
-require('dotenv').config();
-import { AppDataSource } from './data-source';
-import cors from 'cors';
-import errorMiddleware from './middlewares/error-middleware';
-import routes from './router/index';
-import bodyParser from 'body-parser';
-import cookieSession from 'cookie-session';
-import passport from 'passport';
-import cookieParser from 'cookie-parser';
+const express = require("express");
+require("dotenv").config();
+import { AppDataSource } from "./data-source";
+import cors from "cors";
+import errorMiddleware from "./middlewares/error-middleware";
+import routes from "./router/index";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+import passport from "passport";
+import cookieParser from "cookie-parser";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -16,27 +16,27 @@ AppDataSource.initialize()
   .then(async () => {
     console.log(`Database init success!`);
   })
-  .catch(error => console.log(error));
+  .catch((error) => console.log(error));
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cookieSession({ secret: process.env.GOOGLE_CLIENT_SECRET }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PATCH,DELETE',
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PATCH,DELETE",
     credentials: true,
   })
 );
 
-app.use('/api', routes);
+app.use("/api", routes);
 
 app.use(errorMiddleware);
 
